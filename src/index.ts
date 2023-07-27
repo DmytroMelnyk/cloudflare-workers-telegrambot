ad36caf269f6e886f2729e668dfbe66815ebc278
Dmytro Melnyk <dmelnyk@darkstoreapp.com>
2023-07-27 22:57:25 +0000

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import './__global';
-import { Bot, webhookCallback } from "grammy/web";
+import { Bot, webhookCallback, Keyboard, } from "grammy/web";
 import { Env } from './env';
 
 export default {
 	fetch(request: Request, env: Env, context: ExecutionContext) {
 		const bot = new Bot(env.TG_BOT_TOKEN);
 		bot.command("start", async (ctx) => {
-			await ctx.reply("Hello, world!");
+			const keyboard = new Keyboard();
+
+			// Sending the component to the user
+			keyboard.text("A").text("B").row();
+
+			await ctx.reply("Hey, pick a time!", {
+				reply_markup: keyboard,
+			});
 		});
 
 		return webhookCallback(bot, "cloudflare-mod")(request, env, context);
